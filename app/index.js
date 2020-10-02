3f3c8c32b28381b29164be50367baa8b994765e2
lika-alika <Banan922!>
2020-10-02 03:13:09 +0300

    language dropdown

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    var select = document.getElementById("select-wrapper");
+    var selectOptions = document.getElementById("select-options");
     var svg = document.getElementById("map");
+    
+    select.addEventListener("click",function (e) {
+        selectOptions.style.visibility === 'hidden' ? selectOptions.style.visibility = 'visible' : selectOptions.style.visibility = 'hidden'
+    });
+    
     var bbox = svg.getBBox();
     svg.setAttribute("width", bbox.width + "px");
     svg.setAttribute("height", bbox.height + "px");
