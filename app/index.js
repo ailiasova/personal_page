22e1d9c10fe6746d9120dd16baf0451a919ad635
Philip Pavo <f.pavo@spt.team>
2020-10-04 12:51:12 +0200

    map

--- app/index.js
+++ app/index.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var select = document.getElementById("select-wrapper");
-    var selectOptions = document.getElementById("select-options");
-    var svg = document.getElementById("map");
-    
+    initLanguageSelect();
+});
+
+const initLanguageSelect = () => {
+    const select = document.getElementById("language-select");
+    const selectOptions = select.querySelector(".select-options");
+
     select.addEventListener("click",function (e) {
         selectOptions.style.visibility === 'hidden' ? selectOptions.style.visibility = 'visible' : selectOptions.style.visibility = 'hidden'
     });
-    
-    var bbox = svg.getBBox();
-    svg.setAttribute("width", bbox.width + "px");
-    svg.setAttribute("height", bbox.height + "px");
-    svg.setAttribute("viewBox", `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`);
-});
+};
